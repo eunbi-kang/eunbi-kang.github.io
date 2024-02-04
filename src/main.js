4672343d4ce275a7caa255a1afaabda0a11a732e
Eunbi-Kang <ebzzang1905@naver.com>
2024-02-05 04:08:18 +0900

    JS-Arrow up

--- src/main.js
+++ src/main.js
@@ -19,15 +19,19 @@ const home = document.querySelector('.home__container');
 const homeHeight = home.offsetHeight; // home Selector의 높이를 받아옴
 // console.log(homeHeight);
 
-document.addEventListener('scroll', function(e) { 
+document.addEventListener('scroll', ()=>{ 
 // setEventListener() : 나중에 작성한 이벤트리스너 1개만 적용됨 But, addEventListener()은 여러개 작성해도 여러 이벤트가 적용됨
-    const homeOpacity = home.style.opacity = 1- window.scrollY/homeHeight;
+   home.style.opacity = 1 - window.scrollY / homeHeight;
+});
 
-  
-    if(homeOpacity < 0){
-        
-        e.preventDefault();
-        
+/*
+ *  Arrow up 버튼을 아래로 스크롤시 투명하게 처리함
+ */
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', ()=>{ 
+    if(window.scrollY > homeHeight / 2){
+        arrowUp.style.opacity = 1;
+    } else {
+        arrowUp.style.opacity = 0;
     }
-    console.log(homeOpacity)
 });
