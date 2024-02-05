ea863624b78fdb04e6ab4563b6e1ec54a6cf30a9
eunbi-kang <ebzzang1905@naver.com>
2024-02-05 23:25:52 +0900

    JS-프로젝트 필터링>Active메뉴 재설정

--- src/projects.js
+++ src/projects.js
@@ -1,24 +1,38 @@
 'use strict';
 
-/**
-  *  프로젝트 필터링 관련 로직 처리 
-  */
+/*
+ *  프로젝트 필터링 관련 로직 처리 
+ */
 const categories = document.querySelector('.categories'); // 카테고리 버튼 클릭시
 const projects = document.querySelectorAll('.project'); // 카테고리 하위 컨텐츠 클릭시
+const projectsContainer = document.querySelector('.projects');
 
-console.log(projects);
 categories.addEventListener('click', function(event){
     const filter = event.target.dataset.category; // 개발자도구에서 값 확인 가능
     if(filter == null){
         return; // 함수에서 아무런 처리도 하지않고 종료해줌.
     }
-    // projects를 forEach를 통하여 순회하면서 project당 한가지씩 일을 처리해줄건데,
-    // forEach에 각각의 프로젝트 아이템을 함수에서 처리해 줄 것이다.
+
+    /* 
+     *  Active 메뉴를 재설정
+     */
+    const active = document.querySelector('.category--selected');
+    active.classList.remove('category--selected');
+    event.target.classList.add('category--selected'); 
+    // 이벤트의 타겟의 대상이되는 것의 클래스리스트에 'category--selected'를 추가해준다.
+    
+    /* 
+     *  프로젝트 필터링
+     */
+    projectsContainer.classList.add('anim-out');
     projects.forEach(project => {
         if(filter ==='all' || filter === project.dataset.type) {
             project.style.display = 'block';
         } else {
             project.style.display = 'none'
-        }
+        } 
     });
+    setTimeout(()=> {
+        projectsContainer.classList.remove('anim-out');
+    }, 250);
 });
