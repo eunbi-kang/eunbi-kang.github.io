21b474118fade8e3b72c0ae8b95332e90e7d6092
eunbi-kang <ebzzang1905@naver.com>
2024-02-07 17:42:13 +0900

    Active Menu 기능 만들기

--- src/active_menu.js
+++ src/active_menu.js
@@ -8,47 +8,55 @@
     - 마지막 contact 섹션이 보여진다면, 가장 마지막 섹션을 선택한다.
 */
 const sectionIds = [
-  "#home",
-  "#about",
-  "#skills",
-  "#work",
-  "#testimonial",
-  "#contact",
-];
-const sections = sectionIds.map((id) => document.querySelector(id));
+    '#home',
+    '#about',
+    '#skills',
+    '#work',
+    '#testimonial',
+    '#contact',
+  ];
+  const sections = sectionIds.map((id) => document.querySelector(id));
+  const navItems = sectionIds.map((id) =>
+    document.querySelector(`[href="${id}"]`)
+  );
+  const visibleSections = sectionIds.map(() => false);
+  let activeNavItem = navItems[0];
 
-const navItems = sectionIds.map((id) =>
-  document.querySelector(`[href="${id}"]`)
-);
-console.log(navItems);
-const visibleSections = sectionIds.map(() => false);
-
-const options = {};
-const observer = new IntersectionObserver(observerCallback, options);
-sections.forEach((section) => observer.observe(section));
-
-function observerCallback(entries) {
-  let selectLastOne; // flag 변수
-
-  entries.forEach((entry) => {
-    const index = sectionIds.indexOf(`#${entry.target.id}`);
-    visibleSections[index] = entry.isIntersecting;
-    selectLastOne =
-      index === sectionIds.length - 1 &&
-      entry.isIntersecting &&
-      entry.intersectionRatio >= 0.99;
-  });
-  console.log(visibleSections);
-  console.log('무조건 라스트 섹션 !!', selectLastOne);
-
-  const navIndex = selectLastOne
-    ? selectIds.length - 1
-    : findFirstIntersecting(visibleSections);
+  const options = {
+    rootMargin: '-20% 0px 0px 0px',
+    threshold: [0, 0.98],
+  };
+  const observer = new IntersectionObserver(observerCallback, options);
+  sections.forEach((section) => observer.observe(section));
+  
+  function observerCallback(entries) {
+    let selectLastOne;
+    entries.forEach((entry) => {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      visibleSections[index] = entry.isIntersecting;
+      selectLastOne =
+        index === sectionIds.length - 1 &&
+        entry.isIntersecting &&
+        entry.intersectionRatio >= 0.99;
+    });
+    console.log(visibleSections);
+    console.log('무조건 라스트 섹션!!', selectLastOne);
+  
+    const navIndex = selectLastOne
+      ? sectionIds.length - 1
+      : findFirstIntersecting(visibleSections);
     console.log(sectionIds[navIndex]);
-}
 
-// 사용자의 보여지는 화면의 제일 상단 화면의 index를 반환해주는 함수
-function findFirstIntersecting(intersections) {
+    const navItem = navItems[navIndex];
+    activeNavItem.classList.remove('active');
+    activeNavItem = navItem;
+    activeNavItem.classList.add('active');
+  }
+  
+  
+  // 사용자의 보여지는 화면의 제일 상단 화면의 index를 반환해주는 함수
+  function findFirstIntersecting(intersections) {
     const index = intersections.indexOf(true);
     return index >= 0 ? index : 0;
-}
+  }
+  
