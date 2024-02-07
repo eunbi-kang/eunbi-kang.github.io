10a9c4d3996993da75663b5be6eea021a3ec549e
eunbi-kang <ebzzang1905@naver.com>
2024-02-07 19:29:53 +0900

    nav item 선택하는 코드 함수로 작성

--- src/active_menu.js
+++ src/active_menu.js
@@ -1,12 +1,3 @@
-/*
-    구현 계획
-    1. 모든 섹션 요소들과 메뉴 아이템들을 가지고 온다. -> querySelector()
-    2. IntersectionObserver를 사용해서 모든 섹션들을 관찰해야한다.
-    3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다.
-    보여지는 섹션 : 
-    - 다수의 섹션이 동시에 보여진다면, 가장 첫번째 섹션을 선택
-    - 마지막 contact 섹션이 보여진다면, 가장 마지막 섹션을 선택한다.
-*/
 const sectionIds = [
     '#home',
     '#about',
@@ -39,18 +30,12 @@ const sectionIds = [
         entry.isIntersecting &&
         entry.intersectionRatio >= 0.99;
     });
-    console.log(visibleSections);
-    console.log('무조건 라스트 섹션!!', selectLastOne);
   
     const navIndex = selectLastOne
       ? sectionIds.length - 1
       : findFirstIntersecting(visibleSections);
-    console.log(sectionIds[navIndex]);
 
-    const navItem = navItems[navIndex];
-    activeNavItem.classList.remove('active');
-    activeNavItem = navItem;
-    activeNavItem.classList.add('active');
+      selectNavItem(navIndex);
   }
   
   
@@ -60,3 +45,12 @@ const sectionIds = [
     return index >= 0 ? index : 0;
   }
   
+  // navItem 선택하는 함수
+  function selectNavItem(index){
+    const navItem = navItems[index];
+    if (!navItem) return;
+
+    activeNavItem.classList.remove('active');
+    activeNavItem = navItem;
+    activeNavItem.classList.add('active');
+  }
